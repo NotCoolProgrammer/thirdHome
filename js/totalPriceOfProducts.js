2519a94839a241c7657c0bdae29742577889e528
Александр <alexandr.tvelenev@mail.ru>
2020-05-10 20:24:06 +0500

    Upd

--- js/totalPriceOfProducts.js
+++ js/totalPriceOfProducts.js
@@ -1,16 +1,19 @@
 function setTotalPriceOfAllProductsInCart () {
     let span = document.getElementsByClassName('simpleCart_total'); 
     $.post('/session', {}, function (data) {
-        let userSession = JSON.parse(data);
-        let userSessionID = userSession.id;
-        $.post('/totalPrice', {userSessionID}, function (data) {
-            let object = JSON.parse(data);
-            let totalPrice = object.sum;
-            if (totalPrice == null) {
-                totalPrice = 0;
-            }
-            span[0].innerHTML = totalPrice;
-        })
+        let session = JSON.parse(data);
+        if (session !== null) {
+            let userSession = JSON.parse(data);
+            let userSessionID = userSession.id;
+            $.post('/totalPrice', {userSessionID}, function (data) {
+                let object = JSON.parse(data);
+                let totalPrice = object.sum;
+                if (totalPrice == null) {
+                    totalPrice = 0;
+                }
+                span[0].innerHTML = totalPrice;
+            })
+        }
     })
 }
 
